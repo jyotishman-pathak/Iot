747717df490738c557d3688aa00b1fc02eee6331
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2025-04-16 01:29:55 +0530

    klp

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
 
+import {PrismaClient} from "@prisma/client"
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
