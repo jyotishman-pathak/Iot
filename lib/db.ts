3b9d4c6b7707be9c38d16f8a8208897202a8709f
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2025-04-17 01:25:59 +0530

    fix: prisma deployment configuration

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
+// hello from the prisma db
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
